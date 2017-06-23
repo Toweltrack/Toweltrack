5b63c20f9303dcadd299d5342d3db13d8454abd1
Todd Meagher <toddme@gher.com>
2017-06-23 13:53:34 -0500

    Changing email recipient

--- contact.php
+++ contact.php
@@ -1,31 +1,31 @@
 <?php
-	
+
 	require 'vendor/autoload.php';
 	use Mailgun\Mailgun;
-	
+
 	$mailgun = new Mailgun("key-a86fa34a3e3b25da325aac3352d282de");
 	$domain = "toweltrack.com";
-	
-	
+
+
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	
+
 		// Vars
 		$name  = strip_tags(trim($_POST["name"]));
 		$name = str_replace(array("\r","\n"),array(" "," "),$name);
 		$phone = strip_tags(trim($_POST['phone']));
 		$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
 		$resort_name = strip_tags(trim($_POST['resort_name']));
-		
-		
+
+
 		if ( empty($name) || empty($phone) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($resort_name) ) {
 	    // Set a 400 (bad request) response code and exit.
 	    http_response_code(400);
 	    echo "Oops! There was a problem with your submission. Please complete all required fields.";
 	    exit;
     }
-		
+
 		// Set the recipient email address.
-    $recipient = "corp@gher.com";
+    $recipient = "corp@toweltrack.com";
 
     // Set the email subject.
     $subject = "TowelTrack - Info Request";
@@ -38,16 +38,16 @@
 
     // Build the email headers.
     $email_headers = "From: $name <$email>";
-		
-		
+
+
 		// Send email with MailGun
 		$mailgun->sendMessage($domain, array('from' => $email, 'to' => $recipient, 'subject' => $subject, 'text' => $email_content));
 		echo "Your message has been sent.";
-	
+
 	} else {
     // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
   }
-	
+
 ?>
