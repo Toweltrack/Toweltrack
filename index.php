78c2f68b0f093c86ec4e7e7be0bf1789a34a80e3
Todd Meagher <toddme@gher.com>
2018-02-23 15:57:18 -0600

    text

--- index.php
+++ index.php
@@ -50,8 +50,8 @@
 	<div class="row">
 		<div class="col-md-8 col-md-push-2 scrollme centered">
 			<h2>Toweltrack is a high tech replacement for any towel card or manual system.</h2>
-			<p>Power up Toweltrack's waterproof tablet and your ready to go. Our simple towel check-in
-				and checkout software only requires a name, room or phone number, and the number of towels
+			<p>Power up Toweltrack's waterproof tablet and your ready to go. Our simple towel tracking
+				software only requires a name, room or phone number, and the number of towels
 				being distributed to the guest. The system keeps an accurate count of the number of
 				towels distributed to the each guest as well as total number of towels distributed each day. 
 			</p>
